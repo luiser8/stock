41f27b6403fde9684be4c9dbc029135378f37eb2
Luis Rondon <leduardo.rondon@gmail.com>
2019-02-06 22:07:55 -0400

    06-02-2019 22:07

--- php_action/getOrderReport.php
+++ php_action/getOrderReport.php
@@ -4,15 +4,17 @@ require_once 'core.php';
 
 if($_POST) {
 
-	$startDate = $_POST['startDate'];
+	$tipo = $_POST['tipo']; //1 = por fecha, 2 por sucursal
+	$sucursal = isset($_POST['sucursal']) ? $_POST['sucursal'] : '';
+	$startDate = isset($_POST['startDate']) ? $_POST['startDate'] : date('m/d/Y');
 	$date = DateTime::createFromFormat('m/d/Y',$startDate);
 	$start_date = $date->format("Y-m-d");
 
-
-	$endDate = $_POST['endDate'];
+	$endDate = isset($_POST['endDate']) ? $_POST['endDate'] : date('m/d/Y');
 	$format = DateTime::createFromFormat('m/d/Y',$endDate);
 	$end_date = $format->format("Y-m-d");
 
+	if($tipo == 1){
 	$sql = "SELECT * FROM orders WHERE order_date >= '$start_date' AND order_date <= '$end_date' and order_status = 1";
 	$query = $connect->query($sql);
 
@@ -47,7 +49,46 @@ if($_POST) {
 	';	
 
 	echo $table;
+	}if($tipo == 2){
+		$sql = "SELECT orders.*, offices.* FROM orders
+				INNER JOIN offices ON orders.offices_id = offices.offices_id 
+				WHERE orders.offices_id = '$sucursal' and orders.order_status = 1";
+	$query = $connect->query($sql);
 
+	$table = '
+	<table border="1" cellspacing="0" cellpadding="0" style="width:100%;">
+		<tr>
+			<th>Fecha</th>
+			<th>Sucursal</th>
+			<th>Cliente </th>
+			<th>Teléfono</th>
+			<th>Total</th>
+		</tr>
+
+		<tr>';
+		$totalAmount = "";
+		while ($result = $query->fetch_assoc()) {
+			$table .= '<tr>
+				<td><center>'.$result['order_date'].'</center></td>
+				<td><center>'.$result['offices_name'].'</center></td>
+				<td><center>'.$result['client_name'].'</center></td>
+				<td><center>'.$result['client_contact'].'</center></td>
+				<td><center>'.$result['grand_total'].'</center></td>
+			</tr>';	
+			$totalAmount += $result['grand_total'];
+		}
+		$table .= '
+		</tr>
+
+		<tr>
+			<td colspan="3"><center>Total</center></td>
+			<td><center>'.$totalAmount.'</center></td>
+		</tr>
+	</table>
+	';	
+
+	echo $table;
+	}
 }
 
 ?>
--- report.php
+++ report.php
@@ -1,3 +1,4 @@
+<?php require_once 'php_action/db_connect.php' ?>
 <?php require_once 'includes/header.php'; ?>
 
 <div class="row">
@@ -8,8 +9,9 @@
 			</div>
 			<!-- /panel-heading -->
 			<div class="panel-body">
-				
+				<div class="col-md-6">Por fecha
 				<form class="form-horizontal" action="php_action/getOrderReport.php" method="post" id="getOrderReportForm">
+				  <input type="hidden" name="tipo" value="1">
 				  <div class="form-group">
 				    <label for="startDate" class="col-sm-2 control-label">Fecha inicial</label>
 				    <div class="col-sm-10">
@@ -28,7 +30,34 @@
 				    </div>
 				  </div>
 				</form>
+				</div>
 
+				<div class="col-md-6">Por sucursal
+				<form class="form-horizontal" action="php_action/getOrderReport.php" method="post" id="getOrderReportForm">
+				 <input type="hidden" name="tipo" value="2">
+				 <div class="form-group">
+	        	<label for="sucursal" class="col-sm-3 control-label">Sucursal</label>
+				    <div class="col-sm-8">
+				      <select class="form-control" id="sucursal" name="sucursal">
+				      	<option value=""> Selecciona</option>
+				      	<?php 
+				      	$sql = "SELECT offices_id, offices_name FROM offices";
+								$result = $connect->query($sql);
+								while($row = $result->fetch_array()) {
+									echo "<option value='".$row[0]."'>".$row[1]."</option>";
+								} // while
+								
+				      	?>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group-->  
+				  <div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" class="btn btn-success" id="generateReportBtn"> <i class="glyphicon glyphicon-ok-sign"></i> Generar Reporte</button>
+				    </div>
+				  </div>
+				</form>
+				</div>
 			</div>
 			<!-- /panel-body -->
 		</div>
